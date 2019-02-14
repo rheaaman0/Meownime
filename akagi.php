950be5befe964542af306ae4179fefaf4ce9f9b1
sinkaroid <bibsindra@gmail.com>
2019-02-14 09:36:52 +0700

    Create akagi.php

---
+++ akagi.php
@@ -0,0 +1,31 @@
+<?php
+require('dom.php');
+$url = "https://meownime.com/gotoubun-yome-sub-indo/";
+$ch = curl_init();
+$timeout = 5;
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+$html = curl_exec($ch);
+curl_close($ch);
+$dom = new DOMDocument();
+$dom2 = new DOMDocument();
+@$dom->loadHTML($html);
+@$dom2->loadHTML($html);
+$nodes2 = $dom2->getElementsByTagName('title');
+foreach($nodes2 as $node2) {
+}
+echo '<center><h2>'.$node2->textContent.'</h2></center>';
+$nodes = $dom->getElementsByTagName('a');
+foreach($nodes as $node) {
+echo'<center><font face=consolas size=2><a href="';
+echo $node->getAttribute('href').'">'.$node->getAttribute('href').'</a></font> ['.$node->textContent.']<br>';
+/*    url only   
+foreach($dom->getElementsByTagName('a') as $link) {
+        # Show the <a href>
+        echo $link->getAttribute('href');
+        echo "<br />";
+}
+*/
+}
+?>
